381510839643650c2f9b7ace039f9c7b2a7eccc6
https://github.com/thaptb/CIS3534CMRepo <thaptb@students.fscj.edu>
2023-04-03 20:22:47 -0400

    Modifying networkFileRW with my name and current date

--- networkFileRW.py
+++ networkFileRW.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #networkFileRW.py
-#Pamela Brauda
-#Thursday, March 3, 2022
+#Tek Thapa
+#Monday April 3, 2023
 #Update routers and switches;
 #read equipment from a file, write updates & errors to file
 
